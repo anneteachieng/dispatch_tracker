9ccc5c7b4e52903dc0d82713d2966bf7b5cc8b68
anneteachieng <achiengomondi02@gmail.com>
2025-08-19 14:39:50 +0300

    adding and listing new clients

--- dispatch_tracker/clients/views.py
+++ dispatch_tracker/clients/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Client
+from django.contrib.auth.decorators import login_required
+from .forms import ClientForm
 
-# Create your views here.
+@login_required
+def clients_list(request):
+    clients = Client.objects.all()
+    return render(request, "clients/list.html", {"clients": clients})
+
+@login_required
+def client_create(request):
+    if request.methood == "POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("clients_list")
+
+        else:
+            form = ClientForm()
+        return render(request, "clients/form.html", {"form": form})
