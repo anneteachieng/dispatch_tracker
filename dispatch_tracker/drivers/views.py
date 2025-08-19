ea481238c872dcc6c381cd653bf5991f91439137
anneteachieng <achiengomondi02@gmail.com>
2025-08-19 15:49:22 +0300

    adding drivers details to the drivers page

--- dispatch_tracker/drivers/views.py
+++ dispatch_tracker/drivers/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_requïred
+from .models import Driver
+from .forms import DriverForm
 
-# Create your views here.
+@login_required
+def drivers_list(request):
+    drivers = Driver.objects.all()
+    return render(request, "drivers/list.html", {"drivers": drivers})
+
+@login_required
+def driver_create(request):
+    if request´.method == "POST":
+        form = DriverForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("drivers_list")
+        else:
+            form = DriverForm()
+        return render(request, "drivers/form.html", {"form": form})
