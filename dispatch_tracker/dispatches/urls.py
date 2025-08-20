8719cc0bfd0b322b32327c37e06446cd244bc604
anneteachieng <achiengomondi02@gmail.com>
2025-08-20 15:12:33 +0300

    templates for creating and listing dispatch

--- dispatch_tracker/dispatches/urls.py
+++ dispatch_tracker/dispatches/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-        path('', views.home, name='home'),
-        ]
+        path('', views.dispatch_list, name='dispatch_list'),
+        path('create/', views.create_dispatch, name='create_dispatch'),
+        path('<int:pk>/', views.dispatch_detail, name='dispatch_detail'),
+]
 
