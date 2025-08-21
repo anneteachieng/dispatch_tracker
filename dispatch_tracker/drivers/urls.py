de2fba26a0624a72a51da03233cbd9492f30b7e8
anneteachieng <achiengomondi02@gmail.com>
2025-08-21 11:40:02 +0300

    adding driver create, edit and delete urls

--- dispatch_tracker/drivers/urls.py
+++ dispatch_tracker/drivers/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from .import views
 
-urlpatterns = []
+urlpatterns = [
+        path('', views.driver_list, name='driver_list'),
+        path('new/', views.driver_create, name='driver_create'),
+        path('<int:pk>/', views.driver_detail, name='driver_detail'),
+        path('<int:pk>/edit/', views.driver_update, name='driver_update'),
+        path('<int:pk>/delete/', views.driver_delete, name='driver_delete')
+        ]
 
