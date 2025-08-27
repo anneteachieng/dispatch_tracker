bc8b744ae132855f3a037e22bc53a88bfa247372
anneteachieng <achiengomondi02@gmail.com>
2025-08-27 18:38:01 +0300

    adding homepage

--- dispatch_tracker/dispatch_tracker/urls.py
+++ dispatch_tracker/dispatch_tracker/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
+    path('', home, name='home'),          # simple homepage
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('clients/', include('clients.urls')),
     path('drivers/', include('drivers.urls')),
     path('dispatches/', include('dispatches.urls')),
-    path('', include('dispatches.urls')),  # Home page redirects to /dispatches/
 ]
